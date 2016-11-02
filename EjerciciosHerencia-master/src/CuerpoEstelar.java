b8442a15c0c6d46ee402d995f4dbe6590248b3c8
kthanid <ale.aagm@unitec.edu>
2016-11-02 15:23:02 -0600

    Add files via upload

---
+++ EjerciciosHerencia-master/src/CuerpoEstelar.java
@@ -0,0 +1,21 @@
+public class CuerpoEstelar {
+	int posicion;
+	int masa;
+	
+	CuerpoEstelar(int posicion, int masa)
+	{
+		this.posicion = posicion;
+		this.masa = masa;
+	}
+	
+	CuerpoEstelar()
+	{
+		posicion = 0;
+		masa = 0;
+	}
+
+	void CuerpoEstelar()
+	{
+	System.out.print("Cuerpo Estelar");
+	}
+}
